ad45614ded6d54944028dc7f73da7994b17aa663
KidSea <594643219@qq.com>
2017-09-24 14:04:45 +0800

    rxjava2demo学习

--- rxjava2demo/src/main/java/com/meizu/yuxuehai/rxjava2demo/MainActivity.java
+++ rxjava2demo/src/main/java/com/meizu/yuxuehai/rxjava2demo/MainActivity.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +31,88 @@ public class MainActivity extends AppCompatActivity {
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                e.onNext(1);
+//                e.onNext(2);
+//                e.onNext(3);
+//                e.onComplete();
+                Log.d(TAG, "Observable thread is : " + Thread.currentThread().getName());
+                Log.d(TAG, "emit 1");
                 e.onNext(1);
-                e.onNext(2);
-                e.onNext(3);
-                e.onComplete();
             }
         });
+
+        observable.subscribe(observer);
+        observable.subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "After observeOn(mainThread), current thread is: " + Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "After observeOn(io), current thread is: " + Thread.currentThread().getName());
+                    }
+                })
+                .subscribe(mConsumer);
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        }).map(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "This is result " + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d(TAG, s);
+            }
+        });
+
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                final List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                return Observable.fromIterable(list).delay(10, TimeUnit.MILLISECONDS);
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d(TAG, s);
+            }
+        });
+
+
     }
+
+    Consumer<Integer> mConsumer = new Consumer<Integer>() {
+        @Override
+        public void accept(Integer integer) throws Exception {
+            Log.d(TAG, "Observer thread is :" + Thread.currentThread().getName());
+            Log.d(TAG, "onNext: " + integer);
+        }
+    };
 
     //创建一个下游 Observer
     Observer<Integer> observer = new Observer<Integer>() {
@@ -53,5 +137,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "complete");
         }
     };
+
+
+
 
 }
